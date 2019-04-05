094bef70db8e4ccfd2610fdd761717962c793266
amshelhack3r <amshelhack3r@gmail.com>
2019-04-05 12:36:23 +0300

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Banner from "./components/Banner";
+import Icon from "./components/Icon";
+import Nav from "./components/Nav";
+import { Container } from "react-bootstrap";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
+import Slider from "./components/Slider";
+require("bootstrap");
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <div className="mt-5" />
+        <Banner />
+        <div className="rounded-btn mx-auto mt-5">
+          <Icon name={faAngleDown} size="3x" />
+        </div>
+        <Nav />
+        <Slider />
+      </Container>
     );
   }
 }
