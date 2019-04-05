f2e63157e7f9b21eaa7e6c779c0785336570e834
amshelhack3r <amshelhack3r@gmail.com>
2019-04-05 12:35:10 +0300

    Rename to icon

--- src/components/Social.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-export default function Social(props) {
-  return <FontAwesomeIcon icon={["fab", props.brand]} size="2x" />;
-}
